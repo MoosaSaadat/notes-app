be91b3be19269ccd22a50c662aca6771a42e2d16
Muhammad Musa <moosasaadat99@gmail.com>
2019-07-28 13:04:00 +0500

    Updates

---
+++ precache-manifest.c63fd901b37bf15715a2bc4f3705e760.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4e68fe38acb6c77abbcf87a90b2bb4",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "157908701119b75ef9b5",
+    "url": "/notes-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "81067cf02e1d183c70eb",
+    "url": "/notes-app/static/js/2.0af5a204.chunk.js"
+  },
+  {
+    "revision": "157908701119b75ef9b5",
+    "url": "/notes-app/static/js/main.228d62bd.chunk.js"
+  },
+  {
+    "revision": "6e2588e379dbc4286e82",
+    "url": "/notes-app/static/js/runtime~main.2c46edbd.js"
+  }
+]);
